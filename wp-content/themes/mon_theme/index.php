1d7f965732737b75e19b5a5e0ed7123fac4e3f25
nbadji <badjiramatoulaye93@gmail.com>
2016-02-16 11:47:00 -0500

    ajout des autres parametres comme l auteur, la date et les comments

--- wp-content/themes/mon_theme/index.php
+++ wp-content/themes/mon_theme/index.php
@@ -1,6 +1,15 @@
 
 <?php get_header(); ?>
 <section>
-    <!--    Contenu des derniers articles -->
+   <?php
+   while (have_posts()) : the_post(); ?>
+    <article>
+        <h2><?php the_title ()?></h2>
+        <p><?php the_excerpt() ?></p>
+        <p>Article écrit par <?php the_author();?> en date de <?php the_date('')?></p>
+        <a href="<?php the_permalink(); ?>" class="more">Lire la suite</a>
+        <p><?php comments_popup_link('Aucun commentaire','1commentaire','%commentaires') ;?></p>
+    </article>
+    <?php endwhile; ?>
 </section>
 <?php get_footer(); ?>
